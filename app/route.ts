dcd9ed678f2ffeeec5391be5a6901c353d1c08a5
Alexander U <lextechspec@gmail.com>
2025-02-02 15:47:50 +0000

    hotfix

--- app/route.ts
+++ app/route.ts
@@ -4,7 +4,7 @@ export async function GET() {
     const response = NextResponse.json({
         email: "lexizuchenna@gmail.com",
         current_datetime: new Date().toISOString(),
-        github_url: "https://github.com/lexizuchenna/hng-backend-0.git"
+        github_url: "https://github.com/lexizuchenna/hng-backend-0"
     });
 
     response.headers.set("Access-Control-Allow-Origin", "*");
